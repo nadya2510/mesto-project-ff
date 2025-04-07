43cc757ba4bfddc4d481fc9bb0a9e7324285a55c
Nadya Nikolaeva <nadya.nik.ven@yandex.ru>
2025-04-07 10:18:00 +0300

    коммит1

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,31 @@
 // @todo: Темплейт карточки
-
 // @todo: DOM узлы
+const placesList = document.querySelector(".places__list");
 
 // @todo: Функция создания карточки
+function addCard(itemCard, deleteCard) {
+  const cardTemplate = document.querySelector("#card-template").content;
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  const nameValue = itemCard.name;
+  const linkValue = itemCard.link;
+
+  cardElement.querySelector(".card__description .card__title").textContent =
+    nameValue;
+  cardElement.querySelector(".card__image").setAttribute("src", linkValue);
+  cardElement.querySelector(".card__image").setAttribute("alt", nameValue);
+  cardElement
+    .querySelector(".card__delete-button")
+    .addEventListener("click", deleteCard);
+  return cardElement;
+}
 
 // @todo: Функция удаления карточки
+function removeCard(evt) {
+  const removeElement = evt.target.parentElement;
+  removeElement.remove();
+}
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach((item) => {
+  placesList.append(addCard(item, removeCard));
+});
